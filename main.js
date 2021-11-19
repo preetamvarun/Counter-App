486cef6922f9ddd2bb9c147681f9371440e4121e
Preetam Varun <preetamarun99@gmail.com>
2021-11-19 19:33:50 +0530

    complete project

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('start adding javascript to the counter app');
+let counter = 0;
+
+const plus = document.getElementById('plus');
+const minus = document.getElementById('minus');
+const reset = document.getElementsByTagName('a')[0];
+const number = document.getElementById('number');
+
+function resetCounter(e){
+    counter = 0;
+    number.innerHTML = counter;
+    e.preventDefault();
+}
+
+function incrementCounter(e){
+    counter++;
+    number.innerHTML = counter;
+    e.preventDefault();
+}
+
+function decrementCounter(e){
+    counter--;
+    number.innerHTML = counter;
+    e.preventDefault();
+}
+
+plus.addEventListener('click', incrementCounter);
+minus.addEventListener('click', decrementCounter);
+reset.addEventListener('click',resetCounter);
